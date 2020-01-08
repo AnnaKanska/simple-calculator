bd91069b9b5967a301224c2e757003a642eb2be4
Anna <ann.kanska@gmail.com>
2020-01-08 17:53:51 +0100

    added state and logic for simple equations(+-*/=)that follow a sequence

--- src/App.js
+++ src/App.js
@@ -2,30 +2,74 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [startingNum, setStartingNum] = useState(0);
+  const [currentNum, setCurrentNum] = useState(0);
+  const [storedNum, setStoredNum] = useState(null);
+  const [operation, setOperation] = useState(null);
+  // const [result, setResult] = useState(null);
 
   const addNumber = i => {
-    setStartingNum(`${startingNum}` + i);
+    if (currentNum === null) {
+      setCurrentNum(i);
+    } else {
+      setCurrentNum(currentNum === 0 ? i : Number(`${currentNum}` + i));
+    }
   };
-
+  console.log(
+    "current-",
+    currentNum,
+    "stored-",
+    storedNum,
+    "operation-",
+    operation
+  );
   const numbers = new Array(10).fill(0);
   const numDisplay = numbers.map((_, i) => (
-    <div
-      key={`num${+i}`}
-      className={`num${+i}`}
-      value={i}
-      onClick={() => addNumber(i)}
-    >
+    <div key={i} className={`section num${+i}`} onClick={() => addNumber(i)}>
       {i}
     </div>
   ));
 
+  const operationClick = e => {
+    setOperation(e);
+    if (currentNum != null && operation === null) {
+      //checking if it's accumulating the first value
+      setStoredNum(currentNum);
+      setCurrentNum(null);
+    }
+  };
+
+  const getResult = () => {
+    setCurrentNum(null);
+    setStoredNum(eval([storedNum, operation, currentNum].join("")));
+    setOperation(null);
+  };
+
+  const displayValue =
+    currentNum !== null ? currentNum : [storedNum, operation];
+
   return (
-    <div>
-      <div>
-        <h1>{`screen - ${startingNum}`}</h1>
-      </div>
+    <div className="main_container">
+      <div className="section display">{displayValue}</div>
       {numDisplay}
+
+      <button onClick={() => operationClick("+")} className="btn plusBtn">
+        +
+      </button>
+      <button onClick={() => operationClick("-")} className="btn minusBtn">
+        -
+      </button>
+      <button
+        onClick={() => operationClick("*")}
+        className="btn multiplicationBtn"
+      >
+        *
+      </button>
+      <button onClick={() => operationClick("/")} className="btn divisionBtn">
+        /
+      </button>
+      <button onClick={getResult} className="btn equalsBtn">
+        =
+      </button>
     </div>
   );
 }
