2fc5c611535eab14afc9933158aa89d22a38d51d
Anna <ann.kanska@gmail.com>
2020-01-09 13:07:13 +0100

    added state to track in which stage of evaluation the user currently is, reorganized code with switch cases

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const stages = {
+  accFirstVal: "accFirstVal",
+  selectOp: "selectOp",
+  accSecondVal: "accSecondVal",
+  displayResult: "displayResult"
+};
+
 function App() {
-  const [currentNum, setCurrentNum] = useState(0);
-  const [storedNum, setStoredNum] = useState(null);
-  const [operation, setOperation] = useState(null);
-  // const [result, setResult] = useState(null);
+  const [calculatorState, setCalculatorState] = useState({
+    currentNum: 0,
+    storedNum: null,
+    operation: null,
+    stage: stages.accFirstVal
+  });
 
   const addNumber = i => {
-    if (currentNum === null) {
-      setCurrentNum(i);
-    } else {
-      setCurrentNum(currentNum === 0 ? i : Number(`${currentNum}` + i));
-    }
+    setCalculatorState(calculatorState => {
+      switch (calculatorState.stage) {
+        case stages.accFirstVal:
+        case stages.accSecondVal:
+          return {
+            ...calculatorState,
+            currentNum: Number(`${calculatorState.currentNum}` + i),
+            storedNum: eval(
+              [
+                calculatorState.storedNum,
+                calculatorState.operation,
+                calculatorState.currentNum
+              ].join("")
+            )
+          };
+        case stages.selectOp:
+          return {
+            ...calculatorState,
+            currentNum: i,
+            stage: stages.accSecondVal
+          };
+        case stages.displayResult:
+          return {
+            ...calculatorState,
+            currentNum: i,
+            stage: stages.accFirstVal,
+            storedNum: null
+          };
+        default:
+          return calculatorState;
+      }
+    });
   };
-  console.log(
-    "current-",
-    currentNum,
-    "stored-",
-    storedNum,
-    "operation-",
-    operation
-  );
+
   const numbers = new Array(10).fill(0);
   const numDisplay = numbers.map((_, i) => (
     <div key={i} className={`section num${+i}`} onClick={() => addNumber(i)}>
@@ -30,23 +59,75 @@ function App() {
   ));
 
   const operationClick = e => {
-    setOperation(e);
-    if (currentNum != null && operation === null) {
-      //checking if it's accumulating the first value
-      setStoredNum(currentNum);
-      setCurrentNum(null);
-    }
+    setCalculatorState(calculatorState => {
+      switch (calculatorState.stage) {
+        case stages.accFirstVal:
+          return {
+            ...calculatorState,
+            storedNum: calculatorState.currentNum,
+            currentNum: null,
+            operation: e,
+            stage: stages.selectOp
+          };
+        case stages.selectOp:
+          return {
+            ...calculatorState,
+            operation: e
+          };
+        case stages.displayResult:
+        case stages.accSecondVal:
+          return {
+            ...calculatorState,
+            storedNum: eval(
+              [
+                calculatorState.storedNum,
+                calculatorState.operation,
+                calculatorState.currentNum
+              ].join("")
+            ),
+            operation: e,
+            currentNum: null,
+            stage: stages.selectOp
+          };
+        default:
+          return calculatorState;
+      }
+    });
   };
 
   const getResult = () => {
-    setCurrentNum(null);
-    setStoredNum(eval([storedNum, operation, currentNum].join("")));
-    setOperation(null);
+    setCalculatorState(calculatorState => {
+      switch (calculatorState.stage) {
+        case stages.selectOp:
+          return {
+            ...calculatorState,
+            currentNum: calculatorState.storedNum,
+            storedNum: null,
+            stage: stages.accFirstVal
+          };
+        default:
+          return {
+            ...calculatorState,
+            currentNum: null,
+            storedNum: eval(
+              [
+                calculatorState.storedNum,
+                calculatorState.operation,
+                calculatorState.currentNum
+              ].join("")
+            ),
+            operation: null,
+            stage: stages.displayResult
+          };
+      }
+    });
   };
-
   const displayValue =
-    currentNum !== null ? currentNum : [storedNum, operation];
-
+    calculatorState.stage === stages.accFirstVal ||
+    calculatorState.stage === stages.accSecondVal
+      ? calculatorState.currentNum
+      : [calculatorState.storedNum, calculatorState.operation];
+  console.log(calculatorState);
   return (
     <div className="main_container">
       <div className="section display">{displayValue}</div>
